c4024d164e443d0dbf4c6746b74178d33864e57d
1storik <provajdertvoj@gmail.com>
2023-10-15 19:07:49 +0300

    Updating Django Models and Add Settings Room

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('get-room/', GetRoom.as_view()),
     path('join-room/', JoinRoom.as_view()),
     path('user-in-room/', UserInRoom.as_view()),
-    path('leave-room/', LeaveRoom.as_view())
+    path('leave-room/', LeaveRoom.as_view()),
+    path('updata-room/', UpdateRoom.as_view())
 ]
--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import {Link, useParams, useNavigate} from "react-router-dom";
 import {Grid, Button, Typography} from "@material-ui/core"
+import CreateRoomPage from "./CreateRoomPage";
 
 function Room({leaveRoomCallback}) {
   const { roomCode } = useParams();
@@ -8,6 +9,7 @@ function Room({leaveRoomCallback}) {
   const [votesToSkip, setVotesToSkip] = useState(2);
   const [guestCanPause, setGuestCanPause] = useState(false);
   const [isHost, setIsHost] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
   const navigate = useNavigate()
 
   useEffect(() => {
@@ -25,6 +27,7 @@ function Room({leaveRoomCallback}) {
           return response.json()
       })
       .then((data) => {
+          console.log(data.is_host)
         setVotesToSkip(data.votes_to_skip);
         setGuestCanPause(data.guest_can_pause);
         setIsHost(data.is_host);
@@ -44,6 +47,50 @@ function Room({leaveRoomCallback}) {
       });
   }
 
+  const updateShowSettings = (value) =>{
+      setShowSettings(value)
+  }
+
+  const renderSettings = () =>{
+      return(
+      <Grid container spacing={1}>
+          <Grid item xs={12} align="center">
+              <CreateRoomPage
+                  update={true}
+                  voteToSkip={votesToSkip}
+                  guestCanPause={guestCanPause}
+                  roomCode={roomCode}
+                  updateCallback={() => {}}
+              />
+          </Grid>
+          <Grid item xs={12} align="center">
+              <Button
+                  variant="contained"
+                  color="secondary"
+                  onClick={() => updateShowSettings(false)}>
+                  Close Settings
+              </Button>
+          </Grid>
+      </Grid>
+      )
+  }
+
+  const renderSettingsButton = () =>{
+      return(
+          <Grid item xs={12} align="center">
+              <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => updateShowSettings(true)}>
+                  Settings
+              </Button>
+          </Grid>
+      )
+  }
+
+  if (showSettings){
+      return renderSettings();
+  }
   return (
       <Grid container spacing={1}>
           <Grid item xs={12} align="center">
@@ -66,6 +113,7 @@ function Room({leaveRoomCallback}) {
                   Host: {isHost.toString()}
               </Typography>
           </Grid>
+          {isHost ? renderSettingsButton() : null}
           <Grid item xs={12} align="center">
               <Button variant="contained" color="secondary" onClick={LeaveButtonPressed}>
                   Leave Room
